a6bce338671b125fc9c10bc8cccef320879785ef
kobe84 <cesarv01@gmail.com>
2013-10-25 13:38:12 +0200

    Initial Commit

---
+++ OpenGLCurso/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  OpenGLCurso
+//
+//  Created by Cesar Luis Valdez on 25/10/13.
+//  Copyright (c) 2013 Cesar Luis Valdez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
